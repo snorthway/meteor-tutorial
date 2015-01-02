875108b502bc68099c0c01436809dd9d5316ef4a
Stephanie Northway <stephanie.northway@students.olin.edu>
2015-01-02 18:19:07 -0500

    added post_item.js, also wow this trickery, I don't pay to learn things on the internet, guess I'll find another tutorial to follow

---
+++ microscope/client/templates/posts/post_item.js
@@ -0,0 +1,8 @@
+Template.postItem.helpers({
+    domain: function() {
+        var a = document.createElement("a");
+        a.href = this.url;
+        console.log(this);
+        return a.hostname;
+    }
+});
