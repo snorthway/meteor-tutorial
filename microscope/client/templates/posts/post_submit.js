df56f89044596cfdc4f6295b0107abcf842c2f26
Stephanie Northway <stephanie.northway@students.olin.edu>
2015-01-03 17:43:53 -0500

    use a method to submit posts

--- microscope/client/templates/posts/post_submit.js
+++ microscope/client/templates/posts/post_submit.js
@@ -7,7 +7,11 @@ Template.postSubmit.events({
             title: $(e.target).find("[name=title]").val()
         };
 
-        post._id = Posts.insert(post);
-        Router.go("postPage", post);
+        Meteor.call("postInsert", post, function(error, result) {
+            if (error) {
+                return alert(error.reason);
+            }
+            Router.go("postPage", {_id: result._id});
+        });
     }
 });
--- microscope/lib/collections/posts.js
+++ microscope/lib/collections/posts.js
@@ -1,7 +1,28 @@
 Posts = new Mongo.Collection("posts");
 
-Posts.allow({
-    insert: function(userId, doc) {
-        return !!userId;
+// Posts.allow({
+//     insert: function(userId, doc) {
+//         return !!userId;
+//     }
+// });
+
+Meteor.methods({
+    postInsert: function(postAttributes) {
+        console.log(check);
+        check(Meteor.userId(), String);
+        check(postAttributes, {
+            title: String,
+            url: String
+        });
+        var user = Meteor.user();
+        var post = _.extend(postAttributes, {
+            userId: user._id,
+            author: user.username,
+            submitted: new Date()
+        });
+        var postId = Posts.insert(post);
+        return {
+            _id: postId
+        };
     }
 });
