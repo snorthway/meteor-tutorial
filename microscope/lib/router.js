d73526c54d163fdb16878c7bb2b3bac37cbd439c
Stephanie Northway <stephanie.northway@students.olin.edu>
2015-01-03 01:02:17 -0500

    routing to a single post page

--- microscope/lib/router.js
+++ microscope/lib/router.js
@@ -1,5 +1,11 @@
 Router.configure({
-    layoutTemplate: "layout"
+    layoutTemplate: "layout",
+    loadingTemplate: "loading",
+    waitOn: function() { return Meteor.subscribe("posts"); }
 });
 
 Router.route("/", {name: "postsList"});
+Router.route("/posts/:_id", {
+    name: "postPage",
+    data: function() { return Posts.findOne(this.params._id); }
+});
