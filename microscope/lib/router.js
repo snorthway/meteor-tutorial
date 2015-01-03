6321056fd7bc2a6af273a2e20f0ebaa27a734065
Stephanie Northway <stephanie.northway@students.olin.edu>
2015-01-03 17:17:18 -0500

    added post submission, removed insecure

--- microscope/lib/router.js
+++ microscope/lib/router.js
@@ -6,9 +6,12 @@ Router.configure({
 });
 
 Router.route("/", {name: "postsList"});
+
 Router.route("/posts/:_id", {
     name: "postPage",
     data: function() { return Posts.findOne(this.params._id); }
 });
 
-Router.onBeforeAction("dataNotFound", {only: "postPage"}); 
+Router.route("/submit", {name: "postSubmit"});
+
+Router.onBeforeAction("dataNotFound", {only: "postPage"});    
