01fe460cea11caa7b147ab50b249b0182e3a0ce1
Stephanie Northway <stephanie.northway@students.olin.edu>
2015-01-04 10:51:03 -0500

    basic upvoting

--- microscope/client/templates/posts/post_item.js
+++ microscope/client/templates/posts/post_item.js
@@ -10,3 +10,10 @@ Template.postItem.helpers({
         return a.hostname;
     }
 });
+
+Template.postItem.events({
+    "click .upvote": function(e) {
+        e.preventDefault();
+        Meteor.call("upvote", this._id);
+    }
+});
