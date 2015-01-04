1bf6dbc1442cb1c14576d09661759f59b705e6ff
Stephanie Northway <stephanie.northway@students.olin.edu>
2015-01-03 20:21:31 -0500

    added comments collectione etc

--- microscope/lib/router.js
+++ microscope/lib/router.js
@@ -3,7 +3,9 @@ Router.configure({
     layoutTemplate: "layout",
     loadingTemplate: "loading",
     notFoundTemplate: "notFound",
-    waitOn: function() { return Meteor.subscribe("posts"); }
+    waitOn: function() { 
+        return [Meteor.subscribe("posts"), Meteor.subscribe("comments")];
+    }
 });
 
 // route
